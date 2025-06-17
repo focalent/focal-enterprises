16d745342bdfb665e44b4a68202f56233bf07d83
focalent <focalenterprises96@gmail.com>
2025-06-17 13:29:44 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Focal Enterprises",
+  description: "Industrial Products Supplier in Bhopal",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, fontFamily: 'sans-serif' }}>
+        {children}
+      </body>
+    </html>
+  );
+}
